d587169d5340ecfafb2f308921deb320f40edbfd
Lizh690 <690135443@qq.com>
2017-04-06 21:31:22 +0800

    lzh 4/6/17

---
+++ app/src/main/java/com/lee/restaurant/Activity/PayActivity.java
@@ -0,0 +1,21 @@
+package com.lee.restaurant.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.lee.restaurant.R;
+
+/**
+ * Created by Lee on 2017/4/6.
+ */
+
+public class PayActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+
+
+    }
+}
